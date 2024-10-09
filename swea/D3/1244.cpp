2c90e0a0a67fd1954834082f81bee0ef47bd2426
zennnooo <1201jih@naver.com>
2024-10-09 23:15:48 +0900

    swea 일부 해결

---
+++ swea/D3/1244.cpp
@@ -0,0 +1,108 @@
+// 1244. [S/W 문제해결 응용] 2일차 - 최대 상금
+
+/////////////////////////////////////////////////////////////////////////////////////////////
+// 기본 제공코드는 임의 수정해도 관계 없습니다. 단, 입출력 포맷 주의
+// 아래 표준 입출력 예제 필요시 참고하세요.
+// 표준 입력 예제
+// int a;
+// float b, c;
+// double d, e, f;
+// char g;
+// char var[256];
+// long long AB;
+// cin >> a;                            // int 변수 1개 입력받는 예제
+// cin >> b >> c;                       // float 변수 2개 입력받는 예제 
+// cin >> d >> e >> f;                  // double 변수 3개 입력받는 예제
+// cin >> g;                            // char 변수 1개 입력받는 예제
+// cin >> var;                          // 문자열 1개 입력받는 예제
+// cin >> AB;                           // long long 변수 1개 입력받는 예제
+/////////////////////////////////////////////////////////////////////////////////////////////
+// 표준 출력 예제
+// int a = 0;                            
+// float b = 1.0, c = 2.0;               
+// double d = 3.0, e = 0.0; f = 1.0;
+// char g = 'b';
+// char var[256] = "ABCDEFG";
+// long long AB = 12345678901234567L;
+// cout << a;                           // int 변수 1개 출력하는 예제
+// cout << b << " " << c;               // float 변수 2개 출력하는 예제
+// cout << d << " " << e << " " << f;   // double 변수 3개 출력하는 예제
+// cout << g;                           // char 변수 1개 출력하는 예제
+// cout << var;                         // 문자열 1개 출력하는 예제
+// cout << AB;                          // long long 변수 1개 출력하는 예제
+/////////////////////////////////////////////////////////////////////////////////////////////
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string num;
+int ans, max_cnt, num_len;
+
+void dfs(int index, int count);
+
+int main(int argc, char** argv)
+{
+	int test_case;
+	int T;
+	/*
+	   아래의 freopen 함수는 input.txt 를 read only 형식으로 연 후,
+	   앞으로 표준 입력(키보드) 대신 input.txt 파일로부터 읽어오겠다는 의미의 코드입니다.
+	   //여러분이 작성한 코드를 테스트 할 때, 편의를 위해서 input.txt에 입력을 저장한 후,
+	   freopen 함수를 이용하면 이후 cin 을 수행할 때 표준 입력 대신 파일로부터 입력을 받아올 수 있습니다.
+	   따라서 테스트를 수행할 때에는 아래 주석을 지우고 이 함수를 사용하셔도 좋습니다.
+	   freopen 함수를 사용하기 위해서는 #include <cstdio>, 혹은 #include <stdio.h> 가 필요합니다.
+	   단, 채점을 위해 코드를 제출하실 때에는 반드시 freopen 함수를 지우거나 주석 처리 하셔야 합니다.
+	*/
+	//freopen("input.txt", "r", stdin);
+	cin>>T;
+	/*
+	   여러 개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
+	*/
+	for(test_case = 1; test_case <= T; ++test_case)
+	{
+
+        cin >> num >> max_cnt;
+
+        num_len = num.length();
+        ans = 0;
+
+        // 탐색을 해야할 듯 함
+        // 모든 경우의 수를 탐색 -> dfs릉 이용
+        // dfs는 index와 교환횟수를 인자로 받을 것
+
+        dfs(0, 0);
+
+        cout << "#" << test_case << " " << ans << endl;
+
+	}
+
+	return 0;//정상종료시 반드시 0을 리턴해야합니다.
+}
+
+void dfs(int index, int count)
+{
+    if (count == max_cnt) { // 교환 종료 
+        ans = max(ans, stoi(num));
+        return;
+    }
+
+    for (int i = index; i < num_len - 1; i++) {
+        for (int j = i + 1; j < num_len; j++) {
+            if (num[i] <= num[j]) {
+                // 더 작아지게는 스왑 x
+                swap(num[i], num[j]); 
+                dfs(i, count + 1); // 교환한 상태로 dfs 확인
+                swap(num[i], num[j]); // 원래대로 돌아옴 
+            }
+
+            // 이미 잘 정렬되어 있어서 변경이 안될 경우, 교환횟수는 만족해야하므로, 끝에서 첫번째 두번째는 변경할 수 있도록
+            if (i == num_len - 2 && j == num_len - 1) {
+                swap(num[i], num[j]); 
+                dfs(i, count + 1); // 교환한 상태로 dfs 확인
+                swap(num[i], num[j]); // 원래대로 돌아옴 
+            }
+        }
+    }
+}
