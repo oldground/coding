15ffa1bd19d9665a3155c71cd21b0434fad1515c
zennnooo <1201jih@naver.com>
2025-05-14 00:20:05 +0900

    fix(cpp_boj-1158.cpp): 다른 방식 추가

--- boj/cpp_boj/SILVER/1158.cpp
+++ boj/cpp_boj/SILVER/1158.cpp
@@ -25,10 +25,47 @@ void func1(void)
   cout << '>';
 }
 
+void func2(void) 
+{
+  int n, k, len=0, mx=5001;
+  int pre[mx];
+  int nxt[mx];
+  vector<int> vec;
+
+  cin >> n >> k;
+  for (int i = 1; i <= n; ++i) {
+    pre[i] = (i==1) ? n : i-1;
+    nxt[i] = (i==n) ? 1 : i+1;
+    ++len;
+  }
+
+  int i=1;
+  for (int cur=1; len!=0; cur=nxt[cur]) {
+    if(i==k) {
+      // 현재의 이전 = 현재의 다음의 이전
+      // 현재의 다음 = 현재의 이전의 다음
+      pre[nxt[cur]] = pre[cur];
+      nxt[pre[cur]] = nxt[cur];
+      vec.push_back(cur);
+      i=1;
+      --len;
+    }
+    else  ++i;
+  }
+
+  cout << '<';
+  for (i=0; i < vec.size(); ++i) {
+    cout << vec[i]; 
+    if (i != vec.size()-1)  cout << ", ";
+  }
+  cout << '>';
+
+}
+
 int main(void)
 {
   ios::sync_with_stdio(0);
   cin.tie(0);
-  func1();
+  func2();
   return 0;
 }
